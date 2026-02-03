19a5d1509a9bc1686500c27e7f5980cae116bd18
Olena <olenkazaria@gmail.com>
2026-02-03 20:09:46 +0100

    sound removal and fix css

--- src/js/lampa.js
+++ src/js/lampa.js
@@ -1,28 +1,21 @@
 const body = document.body;
 const loginForm = document.getElementById('login-form');
 const pull = document.getElementById('pull');
-const clickSound = document.getElementById('click-sound');
 const lampChain = document.querySelector('.lamp-chain');
 
 let isOn = false;
-
-
 body.classList.remove('lights-on');
 
 function toggleLamp() {
     isOn = !isOn;
-    clickSound.currentTime = 0;
-    clickSound.play();
-
     body.classList.toggle('lights-on', isOn);
-
+    
     if (isOn) {
         gsap.to(body, { backgroundColor: "#1c1f24", duration: 0.6 });
     } else {
         gsap.to(body, { backgroundColor: "#121417", duration: 0.6 });
     }
 }
-
 
 Draggable.create(pull, {
     type: "y",
@@ -34,7 +27,6 @@ Draggable.create(pull, {
         if (this.y > 30) {
             toggleLamp();
         }
-
         gsap.to(this.target, { y: 0, duration: 0.6, ease: "elastic.out(1, 0.3)" });
         gsap.to(lampChain, { height: 100, duration: 0.6 });
     }
