7656c2604f12fceb0ef12d25710b1ac02750ed05
Olena <olenkazaria@gmail.com>
2026-02-03 19:56:25 +0100

    add lampLogin

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/login_with_lampa/', 
     root: 'src',
     build: {
       sourcemap: true,
